58799cb24e23a73094a02a6425c923a0edd5e2f7
MOHD ZAKI BIN ABDULLAH <67680163+mzakiab@users.noreply.github.com>
2024-08-17 00:07:22 +0800

    Update kipas.py
    update nak guna relay 3.3v dan tambah LED Hijau

--- kipas.py
+++ kipas.py
@@ -4,6 +4,7 @@
 # Sistem ini akan feed datanya kepada IoT Platfrom ThingSpeak, bila suhu sampai
 # atau lebih 50c, ThingSpeak akan hantar tweet ke Twitter @Abe_key. Sila rujuk 
 # file iot.py untuk details.
+# Nak guna relay 1 channel 3.3V - 17 / Ogos / 2024 (12:00am)
 
 # Ditulis pada 14 Mei, 2020 bersama Abe Din (9M2CIO)
 # de 9W2KEY OJ15dx
@@ -18,8 +19,9 @@ import re
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.OUT) # Output data untuk kipas sambung ke kaki tengah NPN transistor
-GPIO.setup(26, GPIO.OUT) # Untuk ON LED, nak royat kipas tengah musing
+GPIO.setup(16, GPIO.OUT) # Output data untuk kipas sambung ke relay 3.3V
+GPIO.setup(20, GPIO.OUT) # Untuk ON LED Merah, nak royat kipas tengah musing
+GPIO.setup(21, GPIO.OUT) # Untuk ON LED Hijau, nak royat kipas tak musing. Suhu CPU okay
 
 #Nak ambik bacaan suhu CPU semasa
 def measure_temp():
@@ -34,18 +36,20 @@ while True:     # Loop sokmo sokmo
 
     print 'Suhu CPU: {}'.format(temp)
 
-#kipas mula musing pada suhu 48.5c dan LED pun nyalo
+#kipas mula musing pada suhu 48.5c dan LED Merah pun nyalo, LED Hijau mapuh
 
     if temp >= 48.5:
         print 'Buka kipas'
-        GPIO.output(14, True)
-        GPIO.output(26, True)
+        GPIO.output(16, True)
+        GPIO.output(20, True)
+        GPIO.output(21, False)
 
-#kipas mapuh tak musing, LED pun mapuh jugok
+#kipas mapuh tak musing, LED Merah pun mapuh jugok, tapi LED Hijau nyala
     else:
         print 'Tutup kipas'
-        GPIO.output(14, False)
-        GPIO.output(26, False)
+        GPIO.output(16, False)
+        GPIO.output(20, False)
+        GPIO.output(21, True)
 
 #tunggu pusingan seterusnya 10 saat
     time.sleep(10)
